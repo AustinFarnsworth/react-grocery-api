b891186b40d2922d6913f3915b3634c9fa9b8bea
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2020-08-17 20:27:34 -0600

    first commit

---
+++ src/ProductsService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+// install axios 'npm install axios'
+// URL for our node express running locally
+const BASE_URL = "http://localhost:3005";
+
+const getProducts = () => {
+  return axios.get(`${BASE_URL}/api/products`);
+};
+
+const createProduct = (product) => {
+  return axios.post(`${BASE_URL}/api/products`, product);
+};
+
+const updateProduct = (productId, product) => {
+  return axios.put(`${BASE_URL}/api/products/${productId}`, product);
+};
+
+const deleteProduct = (productId) => {
+  return axios.delete(`${BASE_URL}/api/products/${productId}`);
+};
+
+export { getProducts, createProduct, updateProduct, deleteProduct };
